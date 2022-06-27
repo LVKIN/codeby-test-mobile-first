02fb753732a71da24aae902cfa30b3c08c7677ab
LVKIN <backuplucas6150@gmail.com>
2022-06-27 14:44:52 -0300

    fix: move and indent the code

--- src/pages/WithoutFreeShipping/styled.js
+++ src/pages/WithoutFreeShipping/styled.js
@@ -3,11 +3,11 @@ import { rem } from "polished";
 
 export const WithoutFreeShippingCart = styled.section`
   background-color: #ffffff;
-  border-radius: ${rem(20)};
   
   @media (min-width: 486px) {
-  overflow-y: auto;
-  max-width: ${rem(640)};
-  margin: 0 auto;
-}
+    border-radius: ${rem(20)};
+    overflow-y: auto;
+    max-width: ${rem(640)};
+    margin: 0 auto;
+  }
 `;
